70e1984cd3800ab33719dc4885676234418de60b
Gulab Chand Meena <133342758+SHARA-01@users.noreply.github.com>
2024-12-02 12:37:50 +0530

    Update  add link of server in useFetchJobs.js

--- src/hooks/useFetchJobs.js
+++ src/hooks/useFetchJobs.js
@@ -1,88 +1,88 @@
 
-const JobPostCreate = async({title, company, location, jobDescription}) => {
-    let AddPost = await fetch('http://localhost:8000/api/v1/jobs', {
+const JobPostCreate = async ({ title, company, location, jobDescription }) => {
+    let AddPost = await fetch('https://alumni-connect-server.onrender.com/api/v1/jobs', {
         method: 'post',
-        body: JSON.stringify({title, company, location, jobDescription }),
+        body: JSON.stringify({ title, company, location, jobDescription }),
         headers: {
             'Content-Type': 'application/json'
         },
-        credentials : "include"
+        credentials: "include"
     });
     if (!AddPost.ok) {
-        return {response:await AddPost.json(), status:AddPost.status , statusText:AddPost.statusText};
+        return { response: await AddPost.json(), status: AddPost.status, statusText: AddPost.statusText };
     }
     return AddPost.json();
-    
+
 }
 
-const GetJobInfo = async(id)=> {
-    let response = await fetch(`http://localhost:8000/api/v1/jobs/${id}`, {
+const GetJobInfo = async (id) => {
+    let response = await fetch(`https://alumni-connect-server.onrender.com/api/v1/jobs/${id}`, {
         method: 'get',
-        credentials : "include"
+        credentials: "include"
     });
     if (!response.ok) {
-        return {response:await response.json(), status:response.status , statusText:response.statusText};
+        return { response: await response.json(), status: response.status, statusText: response.statusText };
     }
     return response.json();
-    
+
 }
-const GetAllJobPost = async() => {
-    let GetPost = await fetch('http://localhost:8000/api/v1/jobs', {
+const GetAllJobPost = async () => {
+    let GetPost = await fetch('https://alumni-connect-server.onrender.com/api/v1/jobs', {
         method: 'get',
-        credentials : "include"
+        credentials: "include"
     });
     if (!GetPost.ok) {
-        return {response:await GetPost.json(), status:GetPost.status , statusText:GetPost.statusText};
+        return { response: await GetPost.json(), status: GetPost.status, statusText: GetPost.statusText };
     }
     return GetPost.json();
-    
+
 }
-const GetJobsWithQUery = async(query) => {
-    let GetPost = await fetch(`http://localhost:8000/api/v1/jobs?query=${query}`, {
+const GetJobsWithQUery = async (query) => {
+    let GetPost = await fetch(`https://alumni-connect-server.onrender.com/api/v1/jobs?query=${query}`, {
         method: 'get',
-        credentials : "include"
+        credentials: "include"
     });
     if (!GetPost.ok) {
-        return {response:await GetPost.json(), status:GetPost.status , statusText:GetPost.statusText};
+        return { response: await GetPost.json(), status: GetPost.status, statusText: GetPost.statusText };
     }
     return GetPost.json();
-    
+
 }
 
-const ActivePost = async(id) => {
-    let checkIsActive = await fetch(`http://localhost:8000/api/v1/jobs/${id}`, {
+const ActivePost = async (id) => {
+    let checkIsActive = await fetch(`https://alumni-connect-server.onrender.com/api/v1/jobs/${id}`, {
         method: 'put',
         credentials: "include"
     });
     if (!checkIsActive.ok) {
-        return {response:await checkIsActive.json(), status:checkIsActive.status , statusText:checkIsActive.statusText};
+        return { response: await checkIsActive.json(), status: checkIsActive.status, statusText: checkIsActive.statusText };
     }
     return checkIsActive.json();
 }
 
-const DeactivePost = async(id) => {
-    let checkIsActive = await fetch(`http://localhost:8000/api/v1/jobs/${id}`, {
+const DeactivePost = async (id) => {
+    let checkIsActive = await fetch(`https://alumni-connect-server.onrender.com/api/v1/jobs/${id}`, {
         method: 'put',
         credentials: "include"
     });
     if (!checkIsActive.ok) {
-        return {response:await checkIsActive.json(), status:checkIsActive.status , statusText:checkIsActive.statusText};
+        return { response: await checkIsActive.json(), status: checkIsActive.status, statusText: checkIsActive.statusText };
     }
     return checkIsActive.json();
 }
 
 
-const DeletePost = async(id) => {
-    let deletePost = await fetch(`http://localhost:8000/api/v1/jobs/${id}`, {
+const DeletePost = async (id) => {
+    let deletePost = await fetch(`https://alumni-connect-server.onrender.com/api/v1/jobs/${id}`, {
         method: 'delete',
         credentials: "include"
     });
     if (!deletePost.ok) {
-        return {response:await deletePost.json(), status:deletePost.status , statusText:deletePost.statusText};
+        return { response: await deletePost.json(), status: deletePost.status, statusText: deletePost.statusText };
     }
     return deletePost.json();
 }
 
 
 
-export {JobPostCreate ,GetAllJobPost, ActivePost, DeletePost, DeactivePost, GetJobInfo, GetJobsWithQUery};
+export { JobPostCreate, GetAllJobPost, ActivePost, DeletePost, DeactivePost, GetJobInfo, GetJobsWithQUery };
